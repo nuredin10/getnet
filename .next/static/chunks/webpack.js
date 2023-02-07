91d195d77d5c4b8a010a1d4a3721f190af3c1a5d
nardos atnaw <nardidumessa@gmail.com>
2023-02-07 18:54:45 +0300

    Force Deployment2

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ff94794b047ca861"; }
+/******/ 		__webpack_require__.h = function() { return "d4b4fa6c521bffe8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
